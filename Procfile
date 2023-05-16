db7513b393fb4ddc2fd9a109124337b0e9ac10d7
kelechi <kelechi.oguh505@gmail.com>
2023-05-16 17:00:46 +0100

    Prepared app to host on Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tourism_lagos.wsgi
--- tourism_lagos/settings.py
+++ tourism_lagos/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lyf0=$c(ntc171h_om#qk_4nzqb26yx4!=$fkd=u&=q439hg^+'
+# SECRET_KEY = 'django-insecure-lyf0=$c(ntc171h_om#qk_4nzqb26yx4!=$fkd=u&=q439hg^+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,9 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    "static/",
-]
+# STATICFILES_DIRS = [
+#     "static/",
+# ]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
